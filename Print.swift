6faf5fe82122cac6df967f573861991c8bc92a7b
Mr. S <matthewhanyao@gmail.com>
2018-04-09 14:04:34 -0400

    made grid nicer

--- Print.swift
+++ Print.swift
@@ -24,32 +24,36 @@ class Grid{
     var grid = [[GO?]](repeating: [GO?](repeating: nil, count: 15), count: 15)
     func formatToGrid(_ value: String)-> String{
         var yes = ""
-        for _ in 0..<9 - value.count{
+        for _ in 0..<10 - value.count{
             yes += " "
         }
         yes += value + "|"
         return yes
     }
     func printGrid(){
+        for _ in 0...164{
+            print("_", terminator: "")
+        }
+        print("_")
         for y in 0...14{
-            var row0 = "_"
+            var row0 = "|"
             var row1 = "|"
             var row2 = "|"
             var row3 = "|"
             var row4 = "|"
             for x in 0...14{
-                row0 += "__________"
+                row4 += "__________|"
                 if let thing = grid[y][x]{
-                    row1 += formatToGrid(thing.name)
-                    row2 += formatToGrid(String(thing.energy))
-                    row3 += formatToGrid("(\(y), \(x))")
-                    row4 += formatToGrid(thing.type)
+                    row0 += formatToGrid(thing.name)
+                    row1 += formatToGrid(String(thing.energy))
+                    row2 += formatToGrid("(\(y), \(x))")
+                    row3 += formatToGrid(thing.type)
                 }
                 else{
-                    row1 += "         |"
-                    row2 += "         |"
-                    row3 += "         |"
-                    row4 += "         |"
+                    row0 += "          |"
+                    row1 += "          |"
+                    row2 += "          |"
+                    row3 += "          |"
                 }
             }
             print(row0)
